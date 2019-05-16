5498d1ce2611836d66fd235b040ac0497b8e7bef
Alessandro C <corbisoft@gmail.com>
2019-05-16 11:40:16 +0200

    gitignore setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='autogpy',
+      description = "autogpy: AutoGnuplot.py - automatic generation of gnuplot figures (including script and data) from python"
+      version='0.1',
+      author='Alessandro Corbetta',
+      author_email='a.corbetta@tue.nl',
+      url='https://github.com/acorbe/autogpy',
+      packages=['autogpy'],
+      license="new BSD",
+      install_requires = ['numpy>=1.8']
+)
