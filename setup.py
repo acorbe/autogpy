1552761f63c76f319a3f03ff542d315193cd427c
Alessandro C <corbisoft@gmail.com>
2019-05-16 13:32:10 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@
 
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
 setup(name='autogpy',
       description = "autogpy: AutoGnuplot.py - automatic generation of gnuplot figures (including script and data) from python",
       version='0.1',
       author='Alessandro Corbetta',
       author_email='a.corbetta@tue.nl',
       url='https://github.com/acorbe/autogpy',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
       packages=['autogpy'],
       license="new BSD",
       install_requires = ['numpy>=1.8']
