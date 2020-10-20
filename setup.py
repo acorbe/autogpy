09dd15d1c19c508c8489cf31282b2db272354967
Alessandro Corbetta <corbisoft@gmail.com>
2020-10-20 20:26:37 +0200

    new minor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(name='autogpy',
       description = "autogpy: AutoGnuplot.py - automatic generation of gnuplot figures from python, including scripts and data.",
-      version='0.3.1',
+      version='0.3.2',
       author='Alessandro Corbetta',
       author_email='a.corbetta@tue.nl',
       url='https://github.com/acorbe/autogpy',
